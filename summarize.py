8b647a8ba730351fb5f94465e60a309e399ae3b0
Chandler Swift <chandler@chandlerswift.com>
2024-06-16 15:48:12 -0500

    Add data summarization script

---
+++ summarize.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import json
+import os
+overall = {}
+ordercount = 0
+directory = input("directory? e.g. 2024: ")
+
+def analyze(name, data):
+    totals = {}
+    for order in data:
+        for itemname, item in order['items'].items():
+            if not itemname in totals:
+                totals[itemname] = 0
+            totals[itemname] += item['quantity']
+    print(f"{name}: {len(data)} orders (${sum([data[0]['items'][name]['cost'] * qty for name, qty in totals.items()]):.2f}): {", ".join([f"{total} {item} (${total * data[0]['items'][item]['cost']:.2f})" for item, total in totals.items()])}")
+
+alldata = []
+for file in os.listdir(directory):
+    if not file.endswith(".json"):
+        continue
+    with open(os.path.join(directory, file)) as f:
+        data = json.load(f)
+    alldata += data
+    analyze(file.removesuffix(".json"), data)
+analyze("total", alldata)
